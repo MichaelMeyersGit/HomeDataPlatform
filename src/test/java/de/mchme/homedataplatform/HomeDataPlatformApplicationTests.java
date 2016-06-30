4e280cf86bf5e9bc58b3df08324e4643d541f27f
EMEYERW <extern.michael.meyer4@volkswagen.de>
2016-06-30 13:38:18 +0200

    Initial Project Setup

---
+++ src/test/java/de/mchme/homedataplatform/HomeDataPlatformApplicationTests.java
@@ -0,0 +1,16 @@
+package de.mchme.homedataplatform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = HomeDataPlatformApplication.class)
+public class HomeDataPlatformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
